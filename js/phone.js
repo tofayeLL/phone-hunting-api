5bb78123edcc5001f41cf22e4add83b76dbc8573
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-27 22:26:01 +0600

    added search input and get result by searching name

--- js/phone.js
+++ js/phone.js
@@ -1,7 +1,7 @@
 
 
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (phoneName) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${phoneName}`);
     const data = await res.json();
     const phones = data.data;
     displayPhones(phones);
@@ -11,11 +11,14 @@ const displayPhones = (phones) => {
     console.log(phones);
     // 1. get where we will append
     const phoneContainer = document.getElementById('phone-container');
+    // clear this phone container div
+    phoneContainer.textContent = '';
+
     phones.forEach(phone => {
         console.log(phone);
         // 2. create a div
-        const phoneCard = document.createElement('div');
-        phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+        const phoneCard = document.createElement('div'); 
+        phoneCard.classList = `card bg-base-100 p-4 shadow-xl`;
         // 3.set inner Html
         phoneCard.innerHTML = `
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
@@ -36,6 +39,23 @@ const displayPhones = (phones) => {
 
 
 
+// handle search button
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhone(searchText);
+    
+}
 
 
-loadPhone();
+
+
+
+
+
+
+
+
+
+
+
